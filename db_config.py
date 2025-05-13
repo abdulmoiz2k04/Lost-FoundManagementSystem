6d9d440f7796643bc078b97d4e394c425a65f193
Abdul Moiz <124672178+abdulmoiz2k04@users.noreply.github.com>
2025-05-13 12:46:02 +0500

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def connect_db():
     return mysql.connector.connect(
-        host="sql12.freesqldatabase.com",
-        user="sql12775243",
-        password="xA3WfCDYSy",
-        database="sql12775243"
+        host="",
+        user="",
+        password="",
+        database=""
     )
